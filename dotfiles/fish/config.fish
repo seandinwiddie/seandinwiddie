c745ddb84e1bc4a0b7160d19ce12955c1e92b3b8
Sean Dinwiddie <seandinwiddie@gmail.com>
2026-02-11 19:50:19 -0800

    chore: backup vscode, starship, and fish configurations

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,45 @@
+# Check if shell is interactive
+if status is-interactive
+  # Automatically start tmux if not already in a tmux session
+  # if test -z "$TMUX"
+  #   tmux attach -t default || tmux new -s default
+  # end
+
+  # Conda initialization
+  if test -f "/Users/seandinwiddie/anaconda3/bin/conda"
+    set __conda_setup "$('/Users/seandinwiddie/anaconda3/bin/conda' 'shell.fish' 'hook' 2> /dev/null)"
+    if test $status -eq 0
+      eval "$__conda_setup"
+    else
+      if test -f "/Users/seandinwiddie/anaconda3/etc/profile.d/conda.fish"
+        source "/Users/seandinwiddie/anaconda3/etc/profile.d/conda.fish"
+      else
+        set -gx PATH "/Users/seandinwiddie/anaconda3/bin:$PATH"
+      end
+    end
+    set -e __conda_setup
+  end
+
+  # Starship prompt initialization
+  starship init fish | source
+end
+
+# Added by Windsurf
+fish_add_path /Users/seandinwiddie/.codeium/windsurf/bin
+
+# pnpm
+set -gx PNPM_HOME "/Users/seandinwiddie/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# The next line updates PATH for the Google Cloud SDK.
+# if [ -f '/usr/local/share/google-cloud-sdk/path.fish.inc' ]; . '/usr/local/share/google-cloud-sdk/path.fish.inc'; end
+# export GOOGLE_CLOUD_PROJECT="YOUR_PROJECT_ID"
+# set -x GEMINI_API_KEY "YOUR_GEMINI_API_KEY"
+
+# Added by Antigravity
+fish_add_path /Users/seandinwiddie/.antigravity/antigravity/bin
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/seandinwiddie/.ghcup/bin $PATH # ghcup-env
